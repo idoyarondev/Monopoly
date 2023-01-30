39fd3f370d55e9d83039a787eb248ae9a58cd800
idoyarondev <idoyaron90@gmail.com>
2023-01-30 13:49:39 +1100

    Initial classes

---
+++ src/Monopoly/Monopoly.java
@@ -0,0 +1,10 @@
+package Monopoly;
+
+public class Monopoly {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ido
+ *
+ */
+module Monopoly {
+}
