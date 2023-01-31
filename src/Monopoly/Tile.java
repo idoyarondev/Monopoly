bede558731b892d7ef3aac47fb8c82b5ddc1ec2b
idoyarondev <idoyaron90@gmail.com>
2023-01-31 14:00:11 +1100

    Tile class

--- src/Monopoly/Tile.java
+++ src/Monopoly/Tile.java
@@ -1,5 +1,36 @@
 package Monopoly;
 
 public class Tile {
-
+	private String colour,name,type;
+	private int price;
+	private Player owner;
+	
+	public Tile(String colour,int price,String name,String type) {
+		this.colour = colour;
+		this.price = price;
+		this.name = name;
+		this.type = type;
+	}
+	
+	/* getters */
+	public String getName() {
+		return name;
+	}
+	
+	public Player getOwner() {
+		return owner;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public String getColour() {
+		return colour;
+	}
+	
+	/* setters */
+	public void setOwner(Player player) {
+		owner = player;
+	}
 }
