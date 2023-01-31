0f264ad891ade563605d74c2e92fe5fed651931e
idoyarondev <idoyaron90@gmail.com>
2023-01-31 13:32:21 +1100

    Player class

--- src/Monopoly/Player.java
+++ src/Monopoly/Player.java
@@ -1,5 +1,49 @@
 package Monopoly;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	
+	private String name;
+	private int currentTileNumber;
+	private int balance;
+	private ArrayList<Tile> tilesOwned;
+	
+	public Player(String playerName) {
+		this.name = playerName;
+		currentTileNumber = 0;
+		balance = 16;
+		tilesOwned = new ArrayList<>();
+	}
+	
+	public void addToTilesOwnedByPlayer(Tile tile) {
+		tilesOwned.add(tile);
+	}
+	
+	/* getters */
+	public String getPlayerName() {
+		return name;
+	}
+	
+	public int getCurrentTileNumber() {
+		return currentTileNumber;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public ArrayList<Tile> getTilesOwnedByPlayer(){
+		return tilesOwned;
+	}
+	
+	/* setters */
+	public void setCurrentTileNumber(int currentTileNumber) {
+		this.currentTileNumber = currentTileNumber;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
 }
