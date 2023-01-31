2814f92a1608f91d7ec1badd84316183ae84ee20
idoyarondev <idoyaron90@gmail.com>
2023-01-31 14:16:52 +1100

    Created Player objects

--- src/Monopoly/Monopoly.java
+++ src/Monopoly/Monopoly.java
@@ -3,7 +3,13 @@ package Monopoly;
 public class Monopoly {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/* I Created an object of the class Player for each Monopoly player and placed them in an array of Player for use during the game */
+		Player peter = new Player("Peter");
+		Player billy = new Player("Billy");
+		Player charlotte = new Player("Charlotte");
+		Player sweedal = new Player("Sweedal");
+		Player[] players = {peter,billy,charlotte,sweedal};
 
 	}
 
